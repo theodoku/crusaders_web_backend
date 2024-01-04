0fbdec1d4adf0de99b630b034432f930c21652c8
theophilus doku <theodoku89@gmail.com>
2024-01-04 10:40:12 +0000

    Modified user.rb and user_representer file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-    
     has_secure_password
     validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
     validates :password, presence: true, length: { minimum: 6 }
+
+    has_many :comments
 end
   
