2f0b7e074cb04b4568d11c270bd9fbf2aa423db7
theophilus doku <theodoku89@gmail.com>
2024-01-06 20:34:15 +0000

    Modify seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,50 @@
 User.create!(username: 'sozo', password: '123456')
+
+Person.create!(
+  name: 'Rhanzu',
+  title: 'Team Leader',
+  photo: 'https://images.pexels.com/photos/773371/pexels-photo-773371.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  user_id: 1
+)
+
+Person.create!(
+  name: 'Remy',
+  title: 'District Leader',
+  photo: 'https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=600',
+  user_id: 1
+)
+
+Person.create!(
+  name: 'Paulinah',
+  title: 'District Leader',
+  photo: 'https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  user_id: 1
+)
+
+Person.create!(
+  name: 'Kakra',
+  title: 'District Leader',
+  photo: 'https://images.pexels.com/photos/936043/pexels-photo-936043.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  user_id: 1
+)
+
+Person.create!(
+  name: 'Mokeona',
+  title: 'District Leader',
+  photo: 'https://images.pexels.com/photos/428333/pexels-photo-428333.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  user_id: 1
+)
+
+Person.create!(
+  name: 'Henry',
+  title: 'District Leader',
+  photo: 'https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  user_id: 1
+)
+
+
+
+
+
+
+
