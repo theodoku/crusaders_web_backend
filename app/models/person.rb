812d78aa2f9b8715c492647fccca08842a592343
theophilus doku <theodoku89@gmail.com>
2024-01-06 20:35:08 +0000

    Create person model

---
+++ app/models/person.rb
@@ -0,0 +1,9 @@
+class Person < ApplicationRecord
+    belongs_to :user
+    
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :title, presence: true, length: { maximum: 100 }
+    validates :photo, presence: true, format: { with: /\Ahttps?:\/\/.*\z/,
+                                                message: 'must be a valid URL' }
+end
+  
