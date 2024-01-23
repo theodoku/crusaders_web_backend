370c9dc0c3947a5a90dcc19b63307ae7286840d9
theophilus doku <theodoku89@gmail.com>
2024-01-23 17:24:40 +0000

    Modify application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,17 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module CrusadersWebBackend
+module BookVehicleRentalBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3001', 'http://localhost:3002'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end   
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,5 +25,10 @@ module CrusadersWebBackend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
   end
 end
