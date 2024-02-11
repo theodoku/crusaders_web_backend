d3b3ae85bb9f53076896c882373e4e2ef7d53a55
theophilus doku <theodoku89@gmail.com>
2024-02-11 18:18:46 +0000

    Modify seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,13 +4,15 @@ Person.create!(
   name: 'Rhanzu',
   title: 'Team Leader',
   photo: 'https://images.pexels.com/photos/773371/pexels-photo-773371.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  biography: 'Biography for Rhanzu. This is a dynamic text that provides more information about the person.',
   user_id: 1
 )
 
 Person.create!(
   name: 'Remy',
   title: 'District Leader',
-  photo: 'https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=600',
+  photo: 'https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  biography: 'Biography for Remy. This is a dynamic text that provides more information about the person.',
   user_id: 1
 )
 
@@ -18,6 +20,7 @@ Person.create!(
   name: 'Paulinah',
   title: 'District Leader',
   photo: 'https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  biography: 'Biography for Paulinah. This is a dynamic text that provides more information about the person.',
   user_id: 1
 )
 
@@ -25,6 +28,7 @@ Person.create!(
   name: 'Kakra',
   title: 'District Leader',
   photo: 'https://images.pexels.com/photos/936043/pexels-photo-936043.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  biography: 'Biography for Kakra. This is a dynamic text that provides more information about the person.',
   user_id: 1
 )
 
@@ -32,6 +36,7 @@ Person.create!(
   name: 'Mokeona',
   title: 'District Leader',
   photo: 'https://images.pexels.com/photos/428333/pexels-photo-428333.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  biography: 'Biography for Mokeona. This is a dynamic text that provides more information about the person.',
   user_id: 1
 )
 
@@ -39,6 +44,7 @@ Person.create!(
   name: 'Henry',
   title: 'District Leader',
   photo: 'https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  biography: 'Biography for Henry. This is a dynamic text that provides more information about the person.',
   user_id: 1
 )
 
